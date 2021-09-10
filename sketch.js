5390eee592946001b6bdbbb8a2f7eb7dff30f743
Felipe Carosella <carosellafelipe20@gmail.com>
2021-09-10 00:02:35 -0300

    a

--- sketch.js
+++ sketch.js
@@ -1,14 +1,13 @@
 var capture;
 let boton1; 
 let boton2;
-let pie;
-let sup;
 let seaRoseLines;
 function preload () {
 // lee el archivo de texto y coloca su contenido en una 
 // matriz de cadena con un elemento de matriz por linea en un archivo fuente
   seaRoseLines = loadStrings ('texto.txt'); // siempre debe estar en preload para que funcione
 }
+
 
 function setup() {
   createCanvas(displayWidth, displayHeight); // crea un lienzo de pantalla completa
