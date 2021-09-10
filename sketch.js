39bc879b148ca5518a5623f45d07ab44beac2af4
Felipe Carosella <carosellafelipe20@gmail.com>
2021-09-10 00:07:08 -0300

    a

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-var capture;
 let boton1; 
 let boton2;
 let seaRoseLines;
@@ -11,37 +10,11 @@ function preload () {
 
 function setup() {
   createCanvas(displayWidth, displayHeight); // crea un lienzo de pantalla completa
-    background(50);  
-    if (isMobileDevice()) {
-        console.log("Es un dispositivo movil");
-          var constraints = {
-                audio: false, // desactiva el audio de la cam o dispositivo movil
-                video: {
-                  facingMode: {
-                    exact: "environment" // accede a la camara frontal
-                      }
-                    }
-              };
-
-  capture = createCapture(constraints); // objeto para control camara frontal mobil
-  capture.hide();
-    } else {
-            console.log("No es un dispositivo movil");
-            capture = createCapture(VIDEO);
-            capture.hide();
-          }
+  background(50);  
   // texto principal a desplegarce
   textSize(16);
   for (var i = 0; i < seaRoseLines.length; i++) {
     fill(128+(i*10));
     text(seaRoseLines[i], 20, 150+i*20);
   }
-  boton1 = createButton('Camara'); // crea boton de captura imagen
-  boton1.position(20, 150+seaRoseLines.length*20); // posicion del boton 
-  boton1.size(100);
-  boton1.mousePressed(segundaPagina); // accion al precionar el boton
-  boton2 = createButton('Camara'); // crea boton de captura imagen
-  boton2.position(0, 200); // posicion del boton 
-  boton2.size(100);
-  boton2.mousePressed(camara); // accion al precionar el boton
-  }
+}
