6d8d819f239e10455e08a664043a62654e670d65
Felipe Carosella <carosellafelipe20@gmail.com>
2021-09-10 01:13:45 -0300

    a

--- sketch1.js
+++ sketch1.js
@@ -1,6 +1,7 @@
 var capture;
-let boton;
-
+let boton1;
+let boton2;
+let boton3;
 
 function preload () {
 // lee el archivo de texto y coloca su contenido en una 
@@ -30,23 +31,22 @@ function setup() {
             capture.hide();
           }
 // botones para acciones
-  boton = [boton1 = createButton('captura'),boton2 = createButton('pausa'),boton3 = createButton('continuar')]
-    // crea boton de captura imagen
-    boton[0].position(displayWidth/2-100/2+100, displayHeight - 65 ); // posicion del boton 
-    boton[0].size(100);
-    boton[0].class("btn btn-warning");
-    boton[0].mousePressed(capturarimagen); // accion al precionar el boton 
-     // crea boton de captura imagen
-    boton[1].position(displayWidth/2-100/2, displayHeight - 65 ); // posicion del boton
-    boton[1].size(100);
-    boton[1].class("btn btn-warning");
-    boton[1].mousePressed(pausa); // accion al precionar el boton 
-     // crea boton de captura imagen
-    boton[2].position(displayWidth/2-100/2-100, displayHeight - 65 ); // posicion del boton
-    boton[2].size(100);
-    boton[2].class("btn btn-warning");
-    boton[2].mousePressed(continuar); // accion al precionar el boton
-    boton.class("btn-group");
+
+  boton1 = createButton('captura'); // crea boton de captura imagen
+  boton1.position(displayWidth/2-100/2+100, displayHeight - 65 ); // posicion del boton 
+  boton1.size(100);
+  boton1.class("btn btn-warning")
+  boton1.mousePressed(capturarimagen); // accion al precionar el boton 
+  boton2 = createButton('pausa'); // crea boton de captura imagen
+  boton2.position(displayWidth/2-100/2, displayHeight - 65 ); // posicion del boton
+  boton2.size(100);
+  boton2.class("btn btn-warning")
+  boton2.mousePressed(pausa); // accion al precionar el boton 
+  boton3 = createButton('continuar'); // crea boton de captura imagen
+  boton3.position(displayWidth/2-100/2-100, displayHeight - 65 ); // posicion del boton
+  boton3.size(100);
+  boton3.class("btn btn-warning")
+  boton3.mousePressed(continuar); // accion al precionar el boton
 }
 
 function pausa() {
