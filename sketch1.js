ab535d082520971b62fefdf485a6b082ec18e721
Felipe Carosella <carosellafelipe20@gmail.com>
2021-09-12 13:05:43 -0300

    AC

--- sketch1.js
+++ sketch1.js
@@ -2,7 +2,7 @@ var capture;
 let boton1;
 let boton2;
 let boton3;
-let result;
+let result = false;
 
 function preload () {
 // lee el archivo de texto y coloca su contenido en una 
@@ -75,7 +75,7 @@ function capturarimagen() {
 }
 
 function draw(){
-  if result = true{
+  if result == true{
     image(capture,100, 100,displayWidth-200,displayWidth-200);
   }
   else{
