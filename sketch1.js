5e00d579e7fec74ba8915ef0819f919db1fa258d
Felipe Carosella <carosellafelipe20@gmail.com>
2021-09-14 17:38:33 -0300

    ac

--- sketch1.js
+++ sketch1.js
@@ -3,14 +3,18 @@ let boton1;
 let boton2;
 let boton3;
 let seccion;
+let titulo;
+let clasificador;
 var result;
 let margen;
 let tamañoB;
+let nombre;
+let porcentaje;
 
 function preload () {
-// lee el archivo de texto y coloca su contenido en una 
-// matriz de cadena con un elemento de matriz por linea en un archivo fuente
-  seaRoseLines = loadStrings ('texto.txt'); // siempre debe estar en preload para que funcione
+  // cargamos el link donde se encuentra nuestro modelo pre entrehado por Teachable Machine 
+  // model.json contiene la arquitectura del modelo utilizada por la biblioteca TensorFlow.js  
+  clasificador = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/y8n73XnCI/'+ 'model.json');
 }
 
 function setup() {
@@ -36,6 +40,7 @@ function setup() {
             capture.hide();
             result = false;
           }
+
   if (result == true){
     margen = 50;
     tamañoB = 100;
@@ -44,15 +49,17 @@ function setup() {
   else{
     margen = 100;
     tamañoB = 300;
-    positionRect = [320*2+100,100] 
+    positionRect = [320*2+200,100] 
   }
 
   fill("#F0DB4F");
   seccion = rect(positionRect[0],positionRect[1],displayWidth-(320*2+margen)-margen,240*2);
-  texto = createP("Clasificar");
-  texto.position(margen,100);
-  texto.style("background-color", "#F0DB4F");
-  texto.style("font-size", "30px");
+
+  titulo = createP("Clasificar");
+  titulo.position(margen,100);
+  titulo.style("background-color", "#F0DB4F");
+  titulo.style("font-size", "30px");
+
   // botones para acciones
   boton1 = createButton('captura'); // crea boton de captura imagen
   boton1.position(displayWidth/2-100/2+100+10, displayHeight - 65 ); // posicion del boton 
@@ -60,18 +67,23 @@ function setup() {
   boton1.style("background-color: #F0DB4F");
   boton1.class("btn")
   boton1.mousePressed(capturarimagen); // accion al precionar el boton 
+
   boton2 = createButton('pausa'); // crea boton de captura imagen
   boton2.position(displayWidth/2-100/2, displayHeight - 65 ); // posicion del boton
   boton2.size(tamañoB);
   boton2.style("background-color: #F0DB4F");
   boton2.class("btn btn-warning")
   boton2.mousePressed(pausa); // accion al precionar el boton 
+
   boton3 = createButton('continuar'); // crea boton de captura imagen
   boton3.position(displayWidth/2-100/2-100-10, displayHeight - 65 ); // posicion del boton
   boton3.size(tamañoB);
   boton3.style("background-color: #F0DB4F");
   boton3.class("btn")
   boton3.mousePressed(continuar); // accion al precionar el boton
+
+ // comienza la clasificacion
+  classifyVideo();
 }
 
 function pausa() {
@@ -91,6 +103,25 @@ function capturarimagen() {
     save("image"+".jpg"); // captura imagen en formato jpg
 }
 
+function classifyVideo() {
+// se comienza con la clasificacion del video  y se obtinen una salida de datos con los resultados obtenidos
+  clasificador.classify(video, gotResults); // se guardan los resultados de la clasificacion en gotResults
+}
+
+// TERCER PASO  se obtienen los resultados de la clsificaion en dos variables
+function gotResults(error, results) {
+    if (error) {
+      console.error(error); // muestra el error encontrado  
+  }
+  // se almacenan los resultados obtenidos en las variables 
+  nombre = results[0].label; // nombre de la clasificacion
+  porcentaje = int((results[0].confidence)*100)+"%"; // % de asierto en la clasificacion
+  textSize(16);
+  text(porcentaje, 320*2+200, 300); // muestra el % de asierto
+  text(nombre, 320*2+200, 200);
+  classifyVideo(); 
+}
+
 function draw(){
   if (result == true){
     image(capture,0, 100,displayWidth-100,displayWidth);
@@ -98,4 +129,5 @@ function draw(){
   else{
     image(capture,100, 100,320*2,240*2);
   }
+
 }
