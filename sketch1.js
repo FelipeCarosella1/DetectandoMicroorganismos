ad754130de7940bea7231c2c808f226c15923978
Patricia Gonella <carosellafelipe@gmail.com>
2021-10-13 09:05:11 -0300

    ac

--- sketch1.js
+++ sketch1.js
@@ -142,7 +142,6 @@ function setup() {
         console.log('geolocation NO funcionando');
         console.log(leerDatos())
     };
-  initMap()
   }
 
 //Funciones de botones
@@ -188,35 +187,6 @@ function gotResults(error, results){
   }
 }
 
-function initMap(){
-    const mappa = new Mappa('Leaflet');;
-    const options = {
-    lat: -34.6075682,
-    lng: -58.4370894,
-    zoom: 8,
-    style: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-    }
-    myMap = mappa.tileMap(options);
-    myMap.overlay(canvas);
-    marcador();
-    myMap.onChange(marcador);
-}
-
-function marcador(){
-    clear()
-    let numRows = datos.getRowCount(); // almacena las filas como datos
-    // almacenamos altitud y longitus en una matriz
-    lat = datos.getColumn("lat"); // usamos el nombre que figura en al tabla exel CSV
-    lon = datos.getColumn("lon"); // usamos el nombre que figura en la tabla exel CSV 
-    img = datos.getColumn("img"); // usamos el nombre que figura en al tabla exel CSV
-    // ciclo repetitivo que recorra todos los datos desde 0 hasta el valor de menor de filas 
-    for (let i = 0; i < numRows; i++) {
-        imagen=createImg(img[i]);
-        imagen.hide();
-        let marcador = myMap.latLngToPixel(lat[i],lon[i]);
-        image(imagen,marcador.x,marcador.y,35,35);        
-    }
-}
 
 function gurdar(lati,long){
 let table;
