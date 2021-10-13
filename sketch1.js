971e6980e4775edcb054b1e8df70d6770719b3ea
Patricia Gonella <carosellafelipe@gmail.com>
2021-10-13 08:53:47 -0300

    ac

--- sketch1.js
+++ sketch1.js
@@ -181,9 +181,18 @@ function gotResults(error, results){
   }
 }
 
-//Mostramos la web cam
-function draw(){
-  image(capture,positionCam[0],positionCam[1],positionCam[2],positionCam[3]);
+function initMap(){
+    const mappa = new Mappa('Leaflet');;
+    const options = {
+    lat: -34.6075682,
+    lng: -58.4370894,
+    zoom: 8,
+    style: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+    }
+    myMap = mappa.tileMap(options);
+    myMap.overlay(canvas);
+    marcador();
+    myMap.onChange(marcador);
 }
 
 function marcador(){
@@ -203,27 +212,32 @@ function marcador(){
 }
 
 function gurdar(lati,long){
-  let table;
-  let newRow
-  table = new p5.Table();
-  table.addColumn('lat');
-  table.addColumn('lon');
-  table.addColumn('img');
-  let numRows = datos.getRowCount(); // almacena las filas como datos
-  // almacenamos altitud y longitus en una matriz
-  lat = datos.getColumn("lat"); // usamos el nombre que figura en al tabla exel CSV
-  lon = datos.getColumn("lon"); // usamos el nombre que figura en la tabla exel CSV 
-  img = datos.getColumn("img"); // usamos el nombre que figura en al tabla exel CSV
-  // ciclo repetitivo que recorra todos los datos desde 0 hasta el valor de menor de filas 
-  for (let i = 0; i < numRows; i++) {
-      newRow = table.addRow();
-      newRow.setNum('lat', lat[i]);
-      newRow.setNum('lon', lon[i]);
-      newRow.setString('img', img[i]);
-  }
-  newRow = table.addRow();
-  newRow.setNum('lat', lati);
-  newRow.setNum('lon', long);
-  newRow.setString('img', "agua_enojada.jpg");
-  saveTable(table, 'cordenadas.csv');
+let table;
+let newRow
+table = new p5.Table();
+table.addColumn('lat');
+table.addColumn('lon');
+table.addColumn('img');
+let numRows = datos.getRowCount(); // almacena las filas como datos
+// almacenamos altitud y longitus en una matriz
+lat = datos.getColumn("lat"); // usamos el nombre que figura en al tabla exel CSV
+lon = datos.getColumn("lon"); // usamos el nombre que figura en la tabla exel CSV 
+img = datos.getColumn("img"); // usamos el nombre que figura en al tabla exel CSV
+// ciclo repetitivo que recorra todos los datos desde 0 hasta el valor de menor de filas 
+for (let i = 0; i < numRows; i++) {
+    newRow = table.addRow();
+    newRow.setNum('lat', lat[i]);
+    newRow.setNum('lon', lon[i]);
+    newRow.setString('img', img[i]);
+}
+newRow = table.addRow();
+newRow.setNum('lat', lati);
+newRow.setNum('lon', long);
+newRow.setString('img', "agua_enojada.jpg");
+saveTable(table, 'cordenadas.csv');
+}
+
+//Mostramos la web cam
+function draw(){
+  image(capture,positionCam[0],positionCam[1],positionCam[2],positionCam[3]);
 }
