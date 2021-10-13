2c42dd76470ea73a7f5df5ab07dc0c4386bc7253
Patricia Gonella <carosellafelipe@gmail.com>
2021-10-13 09:15:44 -0300

    ac

--- sketch.js
+++ sketch.js
@@ -1,39 +1,95 @@
-let boton1; 
-let boton2;
-let seaRoseLines;
-let margen
-function preload () {
-// lee el archivo de texto y coloca su contenido en una 
-// matriz de cadena con un elemento de matriz por linea en un archivo fuente
-  seaRoseLines = loadStrings ('texto.txt'); // siempre debe estar en preload para que funcione
-}
+var lati; // variable de latitud
+var long; // variable de longitud
+let canvas;
+let myMap;
+let datos; // variable que almacena los datos del archivo CSV
+let imagen;
+let lat;
+let lon;
+let img;
 
+
+function preload() {
+// el primer comoponete es el archivo csv , el segundo es el tipo de archivo
+// el tercer componente es el encabezado del archivo exel de datos csv
+// asi los datos estaran cargados antes de ejecutar las demas instrucciones
+  datos = loadTable("cordenadas.csv", "csv", "header"); // almacenamos los datos en la variable
+
+}
 
 function setup() {
-  createCanvas(displayWidth, displayHeight); // crea un lienzo de pantalla completa
-  background("#2B2B2B"); 
-  if (isMobileDevice()) {
-        console.log("Es un dispositivo movil");
-        margen = 200;
+    canvas = createCanvas(displayWidth,displayHeight); // crea el linezo de 200x200}
+    rectangulo = rect(0,0,100,100)
+    if('geolocation' in navigator) {
+        /* geolocation is available */
+        console.log('geolocation funcionando');
+        // getCurrentPosition() se usa para obtener la posicion de un dispositivo 
+        navigator.geolocation.getCurrentPosition(async position => {
+            // console.log(position);
+            lati = position.coords.latitude; // obtenemos latitud
+            long = position.coords.longitude; // obtenermos longitud
+            gurdar(lati,long)
+            });
     } else {
-            console.log("No es un dispositivo movil");
-            margen = 0;
-          }
-  texto = createP("Clasificar");
-  texto.position(50,100);
-  texto.style("background-color", "#F0DB4F");
-  texto.style("font-size", "30px");
-  // texto principal a desplegarce
-  textSize(16);
-  for (var i = 0; i < seaRoseLines.length; i++) {
-    if (i > displayWidth-margen){
-      i = 0
+        /* geolocation IS NOT available */
+        console.log('geolocation NO funcionando');
+        console.log(leerDatos())
+    };
+    initMap()
     }
-  }
-  text(seaRoseLines[i], 20, 150+i*20);
+
+function initMap(){
+    const mappa = new Mappa('Leaflet');;
+    const options = {
+    lat: -34.6075682,
+    lng: -58.4370894,
+    zoom: 8,
+    style: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+    }
+    myMap = mappa.tileMap(options);
+    myMap.overlay(canvas);
+    marcador();
+    myMap.onChange(marcador);
 }
 
-//  define si es pantalla de PC o dispostivo mobil 
-function isMobileDevice() {
-    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-};
+function marcador(){
+    clear()
+    let numRows = datos.getRowCount(); // almacena las filas como datos
+    // almacenamos altitud y longitus en una matriz
+    lat = datos.getColumn("lat"); // usamos el nombre que figura en al tabla exel CSV
+    lon = datos.getColumn("lon"); // usamos el nombre que figura en la tabla exel CSV 
+    img = datos.getColumn("img"); // usamos el nombre que figura en al tabla exel CSV
+    // ciclo repetitivo que recorra todos los datos desde 0 hasta el valor de menor de filas 
+    for (let i = 0; i < numRows; i++) {
+        imagen=createImg(img[i]);
+        imagen.hide();
+        let marcador = myMap.latLngToPixel(lat[i],lon[i]);
+        image(imagen,marcador.x,marcador.y,35,35);        
+    }
+}
+
+function gurdar(lati,long){
+    let table;
+    let newRow
+    table = new p5.Table();
+    table.addColumn('lat');
+    table.addColumn('lon');
+    table.addColumn('img');
+    let numRows = datos.getRowCount(); // almacena las filas como datos
+    // almacenamos altitud y longitus en una matriz
+    lat = datos.getColumn("lat"); // usamos el nombre que figura en al tabla exel CSV
+    lon = datos.getColumn("lon"); // usamos el nombre que figura en la tabla exel CSV 
+    img = datos.getColumn("img"); // usamos el nombre que figura en al tabla exel CSV
+    // ciclo repetitivo que recorra todos los datos desde 0 hasta el valor de menor de filas 
+    for (let i = 0; i < numRows; i++) {
+        newRow = table.addRow();
+        newRow.setNum('lat', lat[i]);
+        newRow.setNum('lon', lon[i]);
+        newRow.setString('img', img[i]);
+    }
+    newRow = table.addRow();
+    newRow.setNum('lat', lati);
+    newRow.setNum('lon', long);
+    newRow.setString('img', "agua_enojada.jpg");
+    saveTable(table, 'cordenadas.csv');
+}
