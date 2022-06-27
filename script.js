174156def6eee81e4d512c61d46c26a9de7ef827
Patricia Gonella <carosellafelipe@gmail.com>
2022-06-27 18:54:16 -0300

    ACT

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+//Se realiza esto para saber si el navegador soporta service worker
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('./sw.js')
-        .then(reg=>console.log('registro de SW exitoso',reg))
-        .catch(err=>console.warn("Error al tratar de registrar el SW",err))
+    navigator.serviceWorker.register('./sw.js')//Registramos service worker
+        .then(reg=>console.log('registro de SW exitoso',reg))//Mensaje por consola si se registra exitosamente
+        .catch(err=>console.warn("Error al tratar de registrar el SW",err))//Mensaje por consolo si no se registra exitosamente
 }
--- sw.js
+++ sw.js
@@ -1,34 +1,36 @@
 ;
+//ESTE ES NUESTRO SERVICE WORKER
 //Asignar nombre y version al cache
-const CACHE_NAME = 'v1_cache_programador_fitness',
-  urlsToCache = [
+const CACHE_NAME = 'v1_cache_Mas_Agua_Limpia_Mas_Vida',//Nombre de cache
+  urlsToCache = [//Son las url que se cachean
     './',
     './script.js',
     './sketch1.js',
     './icon.png'
-  ]
+  ] //Se guardan los
 
-//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+//3 EVENTOS DEL SERVICE WORKER
+
+//Durante la fase de instalación, generalmente se almacena en caché los activos estáticos
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME)
+    caches.open(CACHE_NAME)//Esperar hasta que el service worker abra nuestro cache
       .then(cache => {
-        return cache.addAll(urlsToCache)
-          .then(() => self.skipWaiting())
+        return cache.addAll(urlsToCache)//Se agrega al cache del dispositivo todas las urls de la variable usrlsToCache
+          .then(() => self.skipWaiting())//Seguir esperando hasta que termine la lista
       })
-      .catch(err => console.log('Falló registro de cache', err))
+      .catch(err => console.log('Falló registro de cache', err))//Si da error enviamos mensaje por consola
   )
 })
 
 //una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
 self.addEventListener('activate', e => {
-  const cacheWhitelist = [CACHE_NAME]
-
+  const cacheWhitelist = [CACHE_NAME] //Se utiliza para saber si cambio el cache
   e.waitUntil(
-    caches.keys()
-      .then(cacheNames => {
+    caches.keys()//Permite ver las llaves del cache que sufrieron modificaciones
+      .then(cacheNames => {//Devuelve el conjunto de archivos del cache
         return Promise.all(
-          cacheNames.map(cacheName => {
+          cacheNames.map(cacheName => {//Permite evaluar cada archivo e cache
             //Eliminamos lo que ya no se necesita en cache
             if (cacheWhitelist.indexOf(cacheName) === -1) {
               return caches.delete(cacheName)
